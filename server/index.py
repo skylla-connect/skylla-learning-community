e8962a92ad72c31c2bb002b40f3a8b9444891755
Namwanza <namwanzaronald4@gmail.com>
2020-10-14 05:18:30 +0300

    Set up the development environment

--- server/index.py
+++ server/index.py
@@ -1,6 +1,29 @@
 # Import flask
-from flask import Flask  
+from flask import Flask 
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "../config.json"
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
+firebase_admin.initialize_app()
+
+db = firestore.client()
+
+doc_ref = db.collection(u'admin').document(u'Ronnie')
+doc_ref.set({
+    u'fname': u'Ronnie',
+    u'lname': u'Namwanza',
+    u'born': 1993
+})
+
+users_ref = db.collection(u'admin')
+docs = users_ref.stream()
+
+for doc in docs:
+    print(f'{doc.id} => {doc.to_dict()}')
+  
+  
 # Setup the flask app by creating an instance of Flask
 app = Flask(__name__, static_url_path='')  
 
