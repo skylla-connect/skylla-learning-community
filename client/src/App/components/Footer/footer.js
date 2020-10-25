9352384a642016e9d94c99df359146e41fba4f5b
Namwanza <namwanzaronald4@gmail.com>
2020-10-25 14:54:23 +0300

    .

--- client/src/App/components/Footer/footer.js
+++ client/src/App/components/Footer/footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
-import './footer.css';
+import './footer.css';  
 
 const Footer = () => {
     return (
