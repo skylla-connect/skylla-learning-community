43371950bf29a6802715bebd4a31bf47fa1d8b1f
M-Jude <judemark2015@outlook.com>
2020-11-05 10:30:09 +0300

    Update profilePage.js

--- client/src/app/screens/Admin/screens/profilePage.js
+++ client/src/app/screens/Admin/screens/profilePage.js
@@ -1,155 +1,84 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import IconButton from '@material-ui/core/IconButton';
-import clsx from 'clsx';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import FormControl from '@material-ui/core/FormControl';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import {
+    Typography, 
+    Divider,
+    Grid,
+    Paper,
+    ButtonBase
+} from '@material-ui/core';
 
-// send user's data into the database
-import firebase from '../../../firebase';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        '& .MuiTextField-root': {
-        margin: theme.spacing(1),
-        width: 200,
-        },
+        backgroundColor: 'white',
+        width: '99%',
+        height: 'auto',
+        margin: '5% 0 0 0'
     },
 
-    margin: {
-        margin: theme.spacing(1),
-    },
-    withoutLabel: {
-        marginTop: theme.spacing(3),
+    BioData: {
+        padding: 20,
+        flexGrow: 1
     },
 
-    textField: {
+    pPic: {
         width: '100%',
+        borderRadius: '50%'
     },
 
-    container: {
-      width: '50%',
-      margin: '30px auto'
+    paper: {
+        padding: theme.spacing(4),
+        margin: 'auto auto auto 0',
+        maxWidth: 500,
     },
 
-    button: {
-        width: '100%',
-        margin: '10px auto'
+    image: {
+        width: 180,
+        height: 180,
     },
-
-    btn: {
-        backgroundColor: '#EDEDED',
-        color: 'black',
-        width: '100%',
-        padding: 15,
-        disply: 'block',
-        textAlign: 'center',
-        marginLeft: '1.7%',
-        marginRight: '1.7%',
-        marginTop: 8
-    }
 }));
 
-export default function TextFieldSizes() {
+export default function ProfilePage() {
     const classes = useStyles();
-    const [values, setValues] = React.useState({
-        name: '',
-        email: '',
-        password: '',
-        showPassword: false,
-    });
-
-  const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
-  };
-
-  const handleClickShowPassword = () => {
-    setValues({ ...values, showPassword: !values.showPassword });
-  };
-
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-  };
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        let addUsers = firebase.db.collection('users').doc('trainee');
-        addUsers.set({
-            email: "test@skyllaconnect.com",
-            name: 'Ronnie Web',
-            password: 'pass001',
-        })
-        .then(documentReference => {
-        console.log('document reference ID', documentReference.id)
-        })
-        .catch(error => {
-        console.log(error.message)
-        })
-    } 
 
   return (
-    <form className={classes.root} Validate autoComplete="off" handleSubmit>
-        <Typography variant="h6" paragraph style={{textAlign: 'center'}}>
-            Create users
-        </Typography>
-        <div className={classes.container}>
-            <TextField
-                required
-                label="Full Name"
-                id="outlined-size-normal"
-                variant="outlined"
-                value={values.name}
-                onChange={handleChange('name')}
-                style={{width: '100%'}}
-            />
-
-            <TextField
-                required
-                label="E-mail Address"
-                id="outlined-size-normal"
-                variant="outlined"
-                value={values.email}
-                style={{width: '100%'}}
-                onChange={handleChange('email')}
-            />
-
-            <div> 
-                <FormControl className={clsx(classes.margin, classes.textField)} variant="outlined">
-                    <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-                    <OutlinedInput
-                        required
-                        id="outlined-adornment-password"
-                        type={values.showPassword ? 'text' : 'password'}
-                        value={values.password}
-                        onChange={handleChange('password')}
-                        endAdornment={
-                        <InputAdornment position="end">
-                            <IconButton
-                            aria-label="toggle password visibility"
-                            onClick={handleClickShowPassword}
-                            onMouseDown={handleMouseDownPassword}
-                            edge="end"
-                            >
-                            {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                            </IconButton>
-                        </InputAdornment>
-                        }
-                        labelWidth={70}
-                    />
-                </FormControl>
-                
-                <Button variant="outlined" className={classes.btn}>
-                    Created
-                </Button>
-            </div>
+    <Paper className={classes.root}>
+        <div className={classes.paper}>
+            <Grid container spacing={2}>
+                <Grid item>
+                    <ButtonBase className={classes.image}>
+                        <img 
+                            className={classes.pPic} 
+                            src={'https://www.pngitem.com/pimgs/m/442-4426913_avatar-icon-png-white-png-download-white-person.png'} 
+                            alt='Profile Pic'
+                        />
+                    </ButtonBase>
+                </Grid>
+                <Grid item xs={12} sm container>
+                    <Grid item xs container direction="column" spacing={2}>
+                        <Grid item xs>
+                            <Typography gutterBottom variant="h4">
+                                John Doe
+                            </Typography>
+                            <Typography variant="h6" gutterBottom>
+                                Systems Administrator
+                            </Typography>
+                            <Typography variant="body2" gutterBottom color="textSecondary">
+                                ID: 003
+                            </Typography>
+                            <Typography variant="body2" style={{ cursor: 'pointer' }}>
+                                jdoe@skyllaconnect.com
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                    {/* <Grid item>
+                    <Typography variant="subtitle1">$19.00</Typography>
+                    </Grid> */}
+                </Grid>
+            </Grid>
         </div>
-    </form>
+        <Divider/>
+    </Paper>
   );
 }
