871a0963330680f35d2d8667861bd6612ace385d
M-Jude <judemark2015@outlook.com>
2020-11-25 13:39:23 +0300

    updates

--- client/src/LiveSupport/Tabs.js
+++ client/src/LiveSupport/Tabs.js
@@ -4,9 +4,10 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
+import {Divider,Typography } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
-import {NavLink} from 'react-router-dom';
+import './LiveSupport.css'
+import Carousel from 'react-bootstrap/Carousel';
 //  import Cart from '../../../client/src/app/components/Module/Module'
 
 
@@ -43,25 +44,48 @@ function a11yProps(index) {
   };
 }
 
-function LinkTab(props) {
-  return (
-    <Tab
-      component="a"
-      backgroundColor='#ddd'
-      color='white'
-      onClick={(event) => {
-        event.preventDefault();
-      }}
-      {...props}
-    />
-  );
-}
+// function LinkTab(props) {
+//   return (
+//     <Tab
+//       component="a"
+//       backgroundColor='#ddd'
+//       color='white'
+//       onClick={(event) => {
+//         event.preventDefault();
+//       }}
+//       {...props}
+//     />
+//   );
+// }
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     backgroundColor:'#ddd' ,
   },
+  carousel: {
+    width: '100%',
+    padding: 20
+},
+carouselImg: {
+    width: '30%',
+    position: 'relative',
+    margin: '0 0 0 10%',
+},
+caption: {
+    width: '35%',
+    height: 'auto',
+    borderRadius: 70,
+    margin: '0 10% 0 0',
+    float: 'right',
+    top: '0%',
+    padding: 40,
+    color: 'auto',
+},
+
+tab: {
+  '&:focus':{outline: 'none'}
+}
 }));
 
 export default function NavTabs() {
@@ -101,28 +125,32 @@ export default function NavTabs() {
           }}
         >
           <Tab 
+            className={classes.tab}
             label="General" 
-            href="/general" 
+            // href="/general" 
             {
               ...a11yProps(0)} 
               style={{color:'black', 
               // marginTop: '15px'
+              border: 'none'
             }}
           />
 
           <Tab 
+            className={classes.tab}
             label="Product &amp; Services" 
-            href="/product" 
+            // href="/product" 
             {...a11yProps(1)} 
             style={{
-              color:'black', 
+              color:'black',
               // marginTop: '15px'
               }} 
           />
 
           <Tab 
+            className={classes.tab}
             label="Purchase Module" 
-            href="/purchase" 
+            // href="/purchase" 
             {...a11yProps(2)} 
             style={{
               color:'black', 
@@ -130,9 +158,10 @@ export default function NavTabs() {
             }}
           />
 
-          <Tab 
+          <Tab
+            className={classes.tab} 
             label="Code &amp; Debug" 
-            href="/code" 
+            // href="/code" 
             {...a11yProps(3)} 
             style={{
               color:'black', 
@@ -143,7 +172,84 @@ export default function NavTabs() {
       </AppBar>
 
       <TabPanel value={value} index={0}>
-        General
+        <div className='para'>
+            <p>Have a general question about Skylla Products or services? You've come to the right place. Click the 'Live Support Online' button
+                to connect with one our helpful support staffers or consult our  knowledge base for an answer.
+            </p>
+        </div>
+
+        <div className='para'>
+            <h3 style={{marginTop:'20px'}}>Relevant Articles</h3>
+            <ul className='para'>
+                <li>aim higher</li>
+                <li>aim higher</li>
+                <li>aim higher</li>
+            </ul>
+        </div>
+
+        <Carousel className={classes.carousel}> 
+          <Carousel.Item >
+              <div className={classes.caption}>
+                  <Typography style={{fontSize: '240%', margin:'auto', color:'auto'}}>How To Get a Reply Fast</Typography>
+                  <Divider style={{margin: '0 0 20px 0'}}/>
+                  <Typography style={{fontSize: '100%', lineHeight: 2, margin:'20px 0 0 0'}}>
+                      When starting a live Chat please indicate the Fullname 
+                      and email address associated with
+                      your Namecheap account. This will help speed up our verification
+                      and investigation processes
+                  </Typography>
+              </div>  
+              <img 
+                  alt=""   
+                  className={classes.carouselImg} 
+                  src='https://skyllaconnect.com/static/media/devops-development-cycles-automation-monitoring-all-steps-software-construction_6280-79-01-01.322b77c7.png'
+              />  
+          </Carousel.Item> 
+
+          <Carousel.Item  >
+              <div className={classes.caption}>
+                  <Typography style={{fontSize: '240%', margin:'auto', color:'auto'}}>More Tips</Typography>
+                  <Divider style={{margin: '0 0 20px 0'}}/>
+                  <Typography style={{fontSize: '100%', lineHeight: 2, margin:'20px 0 0 0'}}>
+                      When starting a live Chat please indicate the Fullname 
+                      and email address associated with
+                      your Namecheap account. This will help speed up our verification
+                      and investigation processes  
+                  </Typography>
+              </div>  
+              <img 
+                  alt=""   
+                  className={classes.carouselImg} 
+                  style={{borderRadius:'7px 0 7px 0'}} 
+                  src='https://skyllaconnect.com/static/media/devops-development-cycles-automation-monitoring-all-steps-software-construction_6280-79-01-01.322b77c7.png'
+              />     
+          </Carousel.Item>
+
+          <Carousel.Item  >
+              <div className={classes.caption}>
+                  <Typography style={{fontSize: '240%', margin:'auto', color:'auto'}}>Give Feedback</Typography>
+                  <Divider style={{margin: '0 0 20px 0'}}/>
+                  <Typography style={{fontSize: '100%', lineHeight: 2, margin:'20px 0 0 0'}}>
+                      When starting a live Chat please indicate the Fullname 
+                      and email address associated with
+                      your Namecheap account. This will help speed up our verification
+                      and investigation processes 
+                  </Typography>
+              </div>  
+              <img 
+                  alt=""  
+                  className={classes.carouselImg}  
+                  src='https://skyllaconnect.com/static/media/devops-development-cycles-automation-monitoring-all-steps-software-construction_6280-79-01-01.322b77c7.png'  
+              />  
+          </Carousel.Item>   
+        </Carousel>
+
+        <div className='para2'>
+            <p style={{margin:'20px 0% 0 15%'}}>
+              Need help? We are always here for you  <button className='butt'>Live Support</button>
+            </p>
+            
+        </div>
       </TabPanel>
 
       <TabPanel value={value} index={1}>
@@ -151,7 +257,7 @@ export default function NavTabs() {
       </TabPanel>
 
       <TabPanel value={value} index={2} >
-        <NavLink to='/cart'>purchase</NavLink>
+        purchase
       </TabPanel>
 
       <TabPanel value={value}index={3}>
