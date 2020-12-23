a24b88278fca2fa66d9eb49fff31322d66953ba8
Namwanza <namwanzaronald4@gmail.com>
2020-12-23 16:25:27 +0300

    modify announcement

--- client/src/App/screens/Trainee/components/views/announcements.js
+++ client/src/App/screens/Trainee/components/views/announcements.js
@@ -6,11 +6,19 @@ import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
 import FirebaseContext from 'firebase'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Footer from '../../../../components/Footer/footer';
+import KeyboardBackspaceIcon from '@material-ui/icons/KeyboardBackspace';
+import Tooltip from '@material-ui/core/Tooltip';
+import Fab from '@material-ui/core/Fab';
+import *as ROUTE from '../../../../config/routes';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: '100%',
-    padding: 20
+    // width: '80%',
+    padding: 20,
+    backgroundColor: 'transparent',
+    border: '5px solid white'
   },
 
   heading: {
@@ -28,6 +36,25 @@ const useStyles = makeStyles((theme) => ({
 
   cAnno: {
       marginBottom: 20,
+  },
+
+  fab: {
+    margin: theme.spacing(2),
+  },
+
+  absolute: {
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    left: theme.spacing(3),
+    zIndex: 999
+  },
+
+  container: {
+    width: '80%',
+    margin: '20px auto',
+    [theme.breakpoints.down('sm')]: {
+      width: '95%'
+    }
   }
 }));
 
@@ -54,30 +81,47 @@ export default function SimpleAccordion() {
   };
 
   return (
-    <div >
-    
-      <Accordion className={classes.root} expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-        <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        aria-controls="panel1a-content"
-        id="panel1a-header"
-        >
-        <Typography className={classes.heading}>ANNOUNCEMENTS</Typography>
-        </AccordionSummary>
-            {annts.map((anno) => (
+    <div>
+       <Link to={ROUTE.TRAINEE}>
+          <Tooltip title="Click to go back" aria-label="add">
+              <Fab color="primary" className={classes.absolute}>
+                  <KeyboardBackspaceIcon />
+              </Fab>
+          </Tooltip>
+      </Link>
+
+      <div className={
+          classes.container
+        }>
+        <Accordion className={classes.root} expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+          <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+          >
+          <Typography className={classes.heading}>
+            ANNOUNCEMENTS
+          </Typography>
+
+          </AccordionSummary>
+            {
+              annts.map((anno) => (
                 <div key={anno.id}>
-                <AccordionDetails >                
+                  <AccordionDetails >                
                     <Typography className={classes.cAnno}gutterBottom>
                         {anno.Content}
                     </Typography>
                     <Typography className={classes.tAnno} gutterBottom>
-                        {new Date(anno.createdAt).toLocaleDateString("en-US")}<br></br>
-                        {anno.Author}
-                </Typography>       
-                </AccordionDetails> 
-            </div>     
+                      {new Date(anno.createdAt).toLocaleDateString("en-US")}<br></br>
+                      {anno.Author}
+                    </Typography>       
+                  </AccordionDetails> 
+              </div>     
             ))}
-      </Accordion>
+        </Accordion>
+      </div>
+
+      <Footer />
     </div>
   );
 }
--- client/src/authenticated-app.js
+++ client/src/authenticated-app.js
@@ -22,6 +22,7 @@ import ManageEnrolledTrainees from './App/screens/Admin/screens/enrolled'
 import numTrainers from './App/screens/Admin/screens/numTrainers';
 import TotalModules from './App/screens/Admin/screens/totalModules';
 import liveClass from './App/screens/Admin/screens/liveClass';
+import Announcement from './App/screens/Trainee/components/views/announcements';
 
 function NetworkError() {
     return (
@@ -86,6 +87,7 @@ function Routes(props) {
                 <Route path={ROUTES.ENROLLED} component={ManageEnrolledTrainees} />
                 <Route path={ROUTES.T_MODULES} component={TotalModules} />
                 <Route path={ROUTES.Live_CLASSES} component={liveClass} />
+                <Route path={ROUTES.ANNOUNCEMENTS} component={Announcement} />
                 <Route path="/cart" component={CartApp} />
 
                 <Route path="/">
